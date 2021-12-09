477ad4a45925f060cd4ba820d799967cfd061b3b
Щелков Алексей Иванович <orenburgstyle@mail.ru>
2021-12-09 22:56:01 +0500

    start

---
+++ StatusService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
+
+namespace IlyaStatusSite
+{
+  public class StatusService
+    {
+        private int _statusCode;
+
+    
+        public int GetStatus()
+        {
+            return _statusCode;
+        }
+
+        public async Task RequestAsync(string href)
+        {
+           
+            if (href != null)
+            {
+                HttpWebResponse response = null;
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(href);
+                    response = (HttpWebResponse)await request.GetResponseAsync();
+                }
+                catch (WebException)
+                {
+                    response = null;
+                }
+
+                finally
+                {
+                    if (response != null)
+                    {
+                        _statusCode = (int)response.StatusCode;
+                    }
+
+
+                }
+            }
+  
+        }
+
+    }
+}
