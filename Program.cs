62777b854edef5e5e1954e886f14a98df2534b6b
Щелков Алексей Иванович <orenburgstyle@mail.ru>
2022-02-20 13:41:18 +0500

    Только возвращение значения

--- Program.cs
+++ Program.cs
@@ -13,22 +13,15 @@ namespace IlyaStatusSite
         /// <summary>
         /// Точка входа
         /// </summary>
-        /// <param name="args">Для адреса сайт ввести например https://google.com , а далее через пробел букву -r для автозавершения (name.exe https://google.com -r)</param>
+        /// <param name="args">Для адреса сайт ввести например https://google.com (name.exe https://google.com)</param>
         static  int Main(string[] args)
         {
             string site = "https://google.com";
-         //   bool ready = true;
+       
             if (args.Length > 0)
             {
                  site = args[0];
 
-               /* if (args.Length > 1 && args[1]=="-r" )
-                { 
-                    ready = false;
-                }
-               */
-
-                   
             }
 
             StatusService service = new StatusService();
@@ -45,11 +38,6 @@ namespace IlyaStatusSite
 
        
 
-            /* if (ready)
-             {
-                 Console.ReadKey();    
-             }
-             */
 
         }
     }
